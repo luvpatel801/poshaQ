d044b05a31ce06d96090a411fd4e70e729a2cf93
Luv Patel <luvpatel801@gmail.com>
2018-08-28 01:15:42 +0530

    Add files via upload

---
+++ output.js
@@ -0,0 +1,19 @@
+const submission = async (client, dbName, collectionName, bucketSize) => { 
+
+  // Use async-await and lodash to complete this task.
+
+  // client = Mongo server,
+
+  // dbName = Your database name,
+
+  // bucketSize = input variable, can be any integer
+
+  // START your Code
+
+    **
+
+    **
+
+  // END your code  
+
+};
